5bfcc3e1a7b2c0ea14ccbddf9cdb80e9b64688ef
Addison Verger <addison.j.verger@gmail.com>
2019-02-12 20:57:49 -0600

    circles rendering

--- circles/App.js
+++ circles/App.js
@@ -1,11 +1,22 @@
 // Render the circles using React!
 const App = (props) => {
-    let { circles } = props;
+    console.log(props)
+    let { circles } = props
     return (
-        <span>circles</span>
+      <div className="d-flex flex-column align-items-center">
+          <Circle circOutput={props}/>
+      </div>
     )
 }
 
-const Circle = null;  // CHALLENGE: Write a separate Circle component for use in the App component
-
-ReactDOM.render(, document.getElementById('circlesContainer'))
+const Circle = (props) => {
+  console.log(props.circOutput)
+  return (
+    props.circOutput.circles.map((e, index) => {
+      return (
+        <div key= {index} style={{width: e.radius * 2, height: e.radius * 2, borderRadius: e.radius, backgroundColor: e.color}}></div>
+      )
+    })
+  )
+}
+// CHALLENGE: Write a separate Circle component for use in the App component
