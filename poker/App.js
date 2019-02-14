504681ceecf38fdd22d1baf67890919408f5c4ea
Addison Verger <addison.j.verger@gmail.com>
2019-02-13 20:55:24 -0600

    poker rendering

--- poker/App.js
+++ poker/App.js
@@ -2,10 +2,21 @@
 const App = (props) => {
     let { cards } = props;
     return (
-        <span>cards</span>
+      <div className="text-center mt-5 d-flex justify-content-center align-items-start">
+          <Card cardInfoArray={props} />
+      </div>
     )
 }
 
 // CHALLENGE: Write a separate Card component for use in the App component
 // TIP: Use props to pass the suit and number to each Card component
-const Card = null;  
+const Card = (props) => {
+  console.log(props.cardInfoArray)
+  return (
+    props.cardInfoArray.cards.map((e, index) => {
+      return (
+        <img key={index} width="80" src={"cards/" + e.value + e.suit + ".png"}/>
+      )
+    })
+  )
+};
